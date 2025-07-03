e75e60749e24ffa49393d897036654f3fb288457
Lia Logotheti <logoevan@gmail.com>
2025-07-03 11:37:55 +0300

    HTML

---
+++ hotels.php
@@ -0,0 +1,21 @@
+<?php
+  if ($_GET["select"] == "1") {
+    echo'Astir Palace Hotel<br>';
+    echo '40 Apollonos Street, Vouliagmeni, Athens, 16671<br>';
+	echo  '+30 21 0890 1000<br>';
+	  
+  }
+  if ($_GET["select"] == "2") {
+    echo 'Four Seasons Hotel George V<br>';
+    echo '31, Avenue George V, 75008 Paris, France <br>';
+    echo '+33 (1) 49 52 70 00 <br>';
+    
+    }
+    if ($_GET["select"] == "3") {
+      echo 'London at Tower Bridge<br>';
+      echo '10 Trinity Square<br>';
+      echo'London<br>';
+      echo '+44 (0)20 3297 9200<br>';
+    }
+  ?>
+   
